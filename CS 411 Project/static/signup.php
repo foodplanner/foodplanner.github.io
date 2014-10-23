6f6a9169a2ab46fd07ff68a3949b7a99d6ab59a9
Hao Zheng <hzheng19@illinois.edu>
2014-10-23 18:29:09 -0500

    no message

--- CS 411 Project/static/signup.php
+++ CS 411 Project/static/signup.php
@@ -1,21 +1,58 @@
 <?php
 $inputuser = $_POST["username"];
 $inputpass = $_POST["password"];
+$inputname = $_POST["name"];
+$inputdiet = $_POST["diet"];
+//$inputpref = $_POST["pref"];
 
 $dbhost = "engr-cpanel-mysql.engr.illinois.edu";
-$dbuser = "yuehu7_group";
+/*$dbuser = "yuehu7_group";
 $dbpwd = "CS411";
-$db = "yuehu7_Menu";
-$connection = mysql_connect($dbhost, $dbuser, $dbpwd);
+$db = "yuehu7_Menu";*/
+$dbuser = "kim186_user1";
+$dbpwd = "cs411";
+$db = "kim186_cs411";
+//$connection = mysql_connect($dbhost, $dbuser, $dbpwd);
+
+
+// Create connection
+$conn = new mysqli($dbhost, $dbuser, $dbpwd, $db);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql =  "INSERT INTO userAccounts(userID, PASSWORD)
+VALUES ('$inputuser', '$inputpass')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
 
 // Check connection
-if (!$connection) {
+/*if (!$connection) {
     die("Connection failed: " . $conn->connect_error);
     mysql_close();
 } 
 @mysql_select_db($db) or ("Database not found");
 
-$query = "SELECT * FROM `userAccounts` WHERE `UserID` = `$inputuser";
+$sql = "INSERT INTO userAccounts(userID, PASSWORD)
+VALUES ('$inputuser', '$inputpass')";
+
+$result = mysql_query($sql);
+if(!$result){
+	die("Username or password is invalid");
+	mysql_close();
+}
+else{
+	echo "Success!!!";
+}*/
+/*$query = "SELECT * FROM `userAccounts` WHERE `UserID` = `$inputuser";
 $querypass = "SELECT * FROM `userAccounts` WHERE `PASSWORD` = `$inputpass";
 
 $result = mysql_query($query);
@@ -39,5 +76,5 @@ if($serveruser && $serverpass){
 	else{
 		echo "Sorry, bad login";
 	}
-}
+}*/
 ?>
