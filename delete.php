be8efb56fceb5adf1c8b6b169781ea6e584d3a58
danhee1012 <christopher.danhee.choi@gmail.com>
2014-10-23 19:10:47 -0500

    a

---
+++ delete.php
@@ -0,0 +1,30 @@
+<?php
+$inputuser = $_POST["user"];
+$inputpass = $_POST["password"];
+
+$dbhost = "engr-cpanel-mysql.engr.illinois.edu";
+$dbuser = "kim186_user1";
+$dbpwd = "cs411";
+$db = "kim186_cs411";
+$connection = mysql_connect($dbhost, $dbuser, $dbpwd);
+
+// Check connection
+if (!$connection) {
+    die("Connection failed.");
+    mysql_close();
+} 
+@mysql_select_db($db) or ("Database not found");
+
+$query = "DELETE FROM userAccounts WHERE UserID = '$inputuser' AND PASSWORD = '$inputpass'";
+
+$result = mysql_query($query);
+
+if(!$result){
+	die("Username or password is not correct");
+	mysql_close();
+}
+else{
+	header('Location: profile.html');
+}
+
+?>
